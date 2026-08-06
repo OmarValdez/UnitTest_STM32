56ecbb6ebcbe0947c0807ddd54a0a2480a960031
Ruben Valdez <19523018+OmarValdez@users.noreply.github.com>
2026-08-06 13:37:34 -0600

    Modificacion del script .bat de covertura

--- tests/run_coverage.bat
+++ tests/run_coverage.bat
@@ -7,11 +7,24 @@ echo ===== Ceedling finalizado. Continuando... =====
 echo ===== Verificando archivos .gcda =====
 dir /s build\test\out\*.gcda
 
+echo ===== Creando directorio de reportes =====
+if not exist build\coverage mkdir build\coverage
+
 echo ===== Generando reporte HTML =====
 D:\msys64\ucrt64\bin\gcovr.exe --root . --object-directory build\test\out\test_led_logic --gcov-executable "D:\msys64\ucrt64\bin\gcov.exe" --html --html-details -o build\coverage\index.html
 
+if %ERRORLEVEL% NEQ 0 (
+    echo ❌ Error al generar reporte HTML
+    exit /b 1
+)
+
 echo ===== Generando reporte XML =====
 D:\msys64\ucrt64\bin\gcovr.exe --root . --object-directory build\test\out\test_led_logic --gcov-executable "D:\msys64\ucrt64\bin\gcov.exe" --xml -o build\coverage\coverage.xml
+
+if %ERRORLEVEL% NEQ 0 (
+    echo ❌ Error al generar reporte XML
+    exit /b 1
+)
 
 echo ===== Verificando reportes =====
 if exist build\coverage\index.html (
