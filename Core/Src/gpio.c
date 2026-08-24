e50798345e256d490d1d434a5dd1ba51be29503a
Ruben Valdez <19523018+OmarValdez@users.noreply.github.com>
2026-08-24 13:08:21 -0600

    Agregar .gitattributes y renormalizar a LF (CRLF rompia scripts bash en CI)

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -62,13 +62,9 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : PB12 */
   GPIO_InitStruct.Pin = GPIO_PIN_12;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
 }
 
